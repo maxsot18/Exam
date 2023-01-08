beddc80b7369995759f08c0ac235eca41a132168
maxsot <sotnikov.maxim3007@gmail.com>
2023-01-08 20:48:50 +0600

    Task01

---
+++ Desktop/GB/Exam/Task01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1. Найти сумму всех элементов,
+//  между введенными числами. 
+//  (Введеные числа тоже суммируем)
+
+int Input(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int value1, int value2)
+{
+    if (value1 == value2)
+    {
+        return value2;
+    }
+    return value1 + Sum(value1 + 1, value2);
+}
+
+int number1 = Input("Enter a first value");
+int number2 = Input("Enter a second value");
+Console.WriteLine($"Sum of values between {number1} and {number2} = {Sum(number1, number2)}");
+
